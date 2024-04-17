0b9af7c77a051305bab18f8725faec500a1eb505
ShrewdLuni <ander113q@gmail.com>
2024-04-17 22:49:58 +0300

    feat: add answer handle template

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import { Navbar } from "./components/Navbar/index";
 import { Credits } from "./components/Credits/index";
 import { Main } from "./components/Main/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [inputType,setInputType] = useState<"Typing" | "Buttons">("Typing")
-  const [questionImageLink,setQuestionImageLink] = useState("https://www.rafaeldejongh.com/wp-content/uploads/2017/08/Synthwave-Neon-80s-Background-Marmoset.jpg")
-  const [questionLabel,setQuestionLabel] = useState("testing")
   const [gameMode,setGameMode] = useState("")
   const [questionLimit,setQuestionLimit] = useState(100)
 
+  const [questionIndex,setQuestionIndex] = useState(0);
+  const [correct,setCorrect] = useState(0);
+
+
+  const [questionImageLink,setQuestionImageLink] = useState("https://www.rafaeldejongh.com/wp-content/uploads/2017/08/Synthwave-Neon-80s-Background-Marmoset.jpg")
+  const [questionLabel,setQuestionLabel] = useState("testing")
+
+  const [userAnswer,setUserAnswer] = useState("");
+
+  useEffect(() => {
+    nextQuestion();
+  }, [questionIndex]); 
+
 
   function nextQuestion() {
-    //getdata
     setQuestionImageLink("data")
     setQuestionLabel("data")
   }
 
+  function handleAnswer() {
+    if(userAnswer == "data.answer"){
+      setCorrect(correct + 1);
+    }
+    else{
+      //add weak spot to db 
+    }
+    setQuestionIndex(questionIndex + 1);
+  }
 
   return (
     <div className="bg-gray-900">

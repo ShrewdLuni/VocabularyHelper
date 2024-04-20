3b7501f8575de6fbe1077bfca3dacffa9b4fd5ef
ShrewdLuni <ander113q@gmail.com>
2024-04-20 17:42:01 +0300

    feat: add custom test duration

---
+++ src/components/Main/components/Header/CustomNumberDialog.tsx
@@ -0,0 +1,37 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import { Wrench } from "lucide-react"
+import { BarItem } from "./BarItm"
+import { Input } from "@/components/ui/input"
+
+interface CustomNumberDialogProps{
+  questionLimit : number
+  setQuestionLimit: (value : number) => void
+}
+
+
+export function CustomNumberDialog({questionLimit,setQuestionLimit} : CustomNumberDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <BarItem icon={Wrench} isActive={questionLimit != 10 && questionLimit != 25 && questionLimit != 50 && questionLimit != 100}/>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Test duration</AlertDialogTitle>
+          <Input type="number" value={questionLimit} onChange={(e) => setQuestionLimit(e.target.valueAsNumber)}/>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction className="w-full">Ok</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
--- src/components/Main/components/Header/index.tsx
+++ src/components/Main/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import { Wrench } from "lucide-react"
 import { BarItem } from "./BarItm"
+import { CustomNumberDialog } from "./CustomNumberDialog"
 
 interface HeaderProps{
   inputType: string
@@ -15,20 +15,20 @@ export const Header = ({inputType,gameMode,questionLimit,setGameMode,setQuestion
   return (
     <div>
       <div className="text-white flex bg-gray-800 rounded-lg text-sm justify-around items-center h-[4vh]">
-        <div>
+        <div className="flex flex-row">
           <BarItem label="Grammar" onClick={() => setGameMode("Grammar")} isActive={gameMode == "Grammar"}/>
           <BarItem label="Vocabulary" onClick={() => setGameMode("Vocabulary")} isActive={gameMode == "Vocabulary"}/>
         </div>
-        <div className="w-[0.5%] w-min-[4px] h-[70%] bg-gray-900 rounded-xl self-center"></div>
-        <div>
+        <div className="min-w-[4px] w-[1%] h-[70%] bg-gray-900 rounded-xl self-center"></div>
+        <div className="flex flex-row">
           <BarItem label="10" isNumber={true} onClick={() => setQuestionLimit(10)} isActive={questionLimit == 10}/>
           <BarItem label="25" isNumber={true} onClick={() => setQuestionLimit(25)} isActive={questionLimit == 25}/>
           <BarItem label="50" isNumber={true} onClick={() => setQuestionLimit(50)} isActive={questionLimit == 50}/>
           <BarItem label="100" isNumber={true} onClick={() => setQuestionLimit(100)} isActive={questionLimit == 100}/>
-          <BarItem icon={Wrench} isActive={questionLimit != 10 && questionLimit != 25 && questionLimit != 50 && questionLimit != 100}/>
+          <CustomNumberDialog questionLimit={questionLimit} setQuestionLimit={setQuestionLimit}/>
         </div>
-        <div className="w-[0.5%] w-min-[4px] h-[70%] bg-gray-900 rounded-xl self-center"></div>
-        <div>        
+        <div className="min-w-[4px] w-[1%] h-[70%] bg-gray-900 rounded-xl self-center"></div>
+        <div className="flex flex-row">         
           <BarItem label="Typing" onClick={() => setInputType("Typing")} isActive={inputType == "Typing"}/>
           <BarItem label="Clicking" onClick={() => setInputType("Buttons")} isActive={inputType == "Buttons"}/>
         </div>
